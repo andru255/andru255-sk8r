54d07ebb7632068172f33d0652181abf46927e60
Mikael Forsberg <mikaelfor@gmail.comi>
2011-01-12 21:13:40 +0100

    Added jslint conf file

--- experiments/box2d/experiment3/js/line.js
+++ experiments/box2d/experiment3/js/line.js
@@ -1,9 +1,14 @@
+function Point(x,y) {
+  this.x = x;
+  this.y = y;
+}
+
 function Line (ctx) {
   this.ctx = ctx;
-};
+}
 
 Line.prototype = {
   draw: function(from, to) {
     //not implemented
   }
-}
+};
