e3d69c09d3bf6ee75dcf9ac4601114f6d91e299f
Mikael Forsberg <mikaelfor@gmail.comi>
2011-01-16 15:23:00 +0100

    Experiment 3 - Draw your own polygons

--- experiments/box2d/experiment3/js/experiment3.js
+++ experiments/box2d/experiment3/js/experiment3.js
@@ -1,4 +1,11 @@
+var world;
 window.onload = function() {
   var canvas = document.getElementById("canvas");
-  var pencil = new Pencil(canvas);
+  world = new World({canvas: canvas});
+  var pencil = new Pencil(world);
 };
+
+function drop() {
+  window.setInterval(world.update.bind(world), 1000 / 30);
+  world.dropBalls();
+}
