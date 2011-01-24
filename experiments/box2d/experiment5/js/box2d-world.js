3522ce3203a05b9223fdd98167dda1a0a473dd11
Mikael Forsberg <mikaelfor@gmail.com>
2011-01-24 19:34:31 +0100

    added actor.js from experiment5

---
+++ experiments/box2d/experiment5/js/box2d-world.js
@@ -0,0 +1,269 @@
+/*
+Copyright 2011 Johan Maasing
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+
+var gameWorld;
+window.onload = function() {
+	var canvas = document.getElementById('canvas');
+	var ctx = canvas.getContext('2d');
+	gameWorld = new World(ctx);
+	gameWorld.init();
+}
+
+function reset() {
+	gameWorld.reset();
+}
+
+// Global constants
+var pixelsPerMeter = 40;
+var worldWidthInMeter = 16; // 640/pixelsPerMeter ;
+var worldHeightInMeter = 12; // 480/pixelsPerMeter ;
+// According to http://defekt.se/2010/04/den-standardiserade-skateboarden/
+var wheelRadius = 0.05;
+var boardThickness = 0.02 ;
+var boardLength = 0.82 ;
+var truckOffset = 0.18 ;
+
+var wheelRadiusInPixels = wheelRadius * pixelsPerMeter ;
+var halfBoardThicknessInPixels = Math.max(1,Math.round(boardThickness/2 * pixelsPerMeter)) ;
+var halfBoardLengthInPixels = Math.max(1,Math.round(boardLength/2 * pixelsPerMeter)) ;
+
+function World(ctx) {
+	this.bodies = new Array();
+	this.joints = new Array();
+	this.world = new Box2D.Dynamics.b2World(
+			new Box2D.Common.Math.b2Vec2(0, 10), true);
+	this.ctx = ctx;
+	this.board;
+}
+
+function bind(obj, fn) {
+	return function() {
+			fn.call(obj) ;
+	};
+}
+
+World.prototype = {
+	init : function() {
+		
+		this.initRamp() ;
+		this.initBodies();
+
+		// setup debug draw
+		var debugDraw = new Box2D.Dynamics.b2DebugDraw();
+		debugDraw.SetSprite(this.ctx);
+		debugDraw.SetDrawScale(pixelsPerMeter);
+		debugDraw.SetFillAlpha(0.6);
+		debugDraw.SetLineThickness(1.0);
+		debugDraw.SetFlags(Box2D.Dynamics.b2DebugDraw.e_shapeBit
+				| Box2D.Dynamics.b2DebugDraw.e_jointBit);
+		this.world.SetDebugDraw(debugDraw);
+		
+		var callback = bind(this, this.update) ;
+		this.timerId = window.setInterval(callback, 1000 / 30);
+	},
+	reset : function() {
+		// Destroy the existing joints
+		for ( var n = 0; n < this.joints.length; n++) {
+			this.world.DestroyJoint(this.joints[n]);
+		}
+		// Destroy the existing bodies
+		for ( var n = 0; n < this.bodies.length; n++) {
+			this.world.DestroyBody(this.bodies[n]);
+		}
+		this.bodies = new Array();
+
+		this.initBodies();
+	},
+	initRamp : function() {
+		var fixDef = new Box2D.Dynamics.b2FixtureDef();
+		fixDef.density = 1.0;
+		fixDef.friction = 0.9;
+		fixDef.restitution = 0.01;
+		
+		var bodyDef = new Box2D.Dynamics.b2BodyDef();
+		bodyDef.type = Box2D.Dynamics.b2Body.b2_staticBody;
+		
+		var vertices = [
+			new Box2D.Common.Math.b2Vec2(0, 0),
+			new Box2D.Common.Math.b2Vec2(1,1),
+			new Box2D.Common.Math.b2Vec2(2, 3),
+			new Box2D.Common.Math.b2Vec2(2, 11),
+			new Box2D.Common.Math.b2Vec2(0, 11)
+		] ;
+		fixDef.shape = Box2D.Collision.Shapes.b2PolygonShape.AsArray(vertices, vertices.length) ;
+		bodyDef.position.x = 0 ;
+		bodyDef.position.y = 1 ;
+		this.world.CreateBody(bodyDef).CreateFixture(fixDef);
+		
+		var vertices = [
+			new Box2D.Common.Math.b2Vec2(0, 0),
+			new Box2D.Common.Math.b2Vec2(2, 4),
+			new Box2D.Common.Math.b2Vec2(2, 6),
+			new Box2D.Common.Math.b2Vec2(0, 6)
+		] ;
+		fixDef.shape = Box2D.Collision.Shapes.b2PolygonShape.AsArray(vertices, vertices.length) ;
+		bodyDef.position.x = 2 ;
+		bodyDef.position.y = 6 ;
+		this.world.CreateBody(bodyDef).CreateFixture(fixDef);
+		
+		var vertices = [
+			new Box2D.Common.Math.b2Vec2(0, 0),
+			new Box2D.Common.Math.b2Vec2(1, 1),
+			new Box2D.Common.Math.b2Vec2(1, 2),
+			new Box2D.Common.Math.b2Vec2(0, 2)
+		] ;
+		fixDef.shape = Box2D.Collision.Shapes.b2PolygonShape.AsArray(vertices, vertices.length) ;
+		bodyDef.position.x = 4 ;
+		bodyDef.position.y = 10 ;
+		this.world.CreateBody(bodyDef).CreateFixture(fixDef);
+		
+		var vertices = [
+			new Box2D.Common.Math.b2Vec2(0, 0),
+			new Box2D.Common.Math.b2Vec2(2, 1),
+			new Box2D.Common.Math.b2Vec2(0, 1)
+		] ;
+		fixDef.shape = Box2D.Collision.Shapes.b2PolygonShape.AsArray(vertices, vertices.length) ;
+		bodyDef.position.x = 5 ;
+		bodyDef.position.y = 11 ;
+		this.world.CreateBody(bodyDef).CreateFixture(fixDef);
+		
+		var vertices = [
+			new Box2D.Common.Math.b2Vec2(0, 0),
+			new Box2D.Common.Math.b2Vec2(2, 0),
+			new Box2D.Common.Math.b2Vec2(2, 1),
+			new Box2D.Common.Math.b2Vec2(0, 1)
+		] ;
+		fixDef.shape = Box2D.Collision.Shapes.b2PolygonShape.AsArray(vertices, vertices.length) ;
+		bodyDef.position.x = 7 ;
+		bodyDef.position.y = 12 ;
+		this.world.CreateBody(bodyDef).CreateFixture(fixDef);
+		
+		var vertices = [
+			new Box2D.Common.Math.b2Vec2(0, 1),
+			new Box2D.Common.Math.b2Vec2(2, 0),
+			new Box2D.Common.Math.b2Vec2(2, 1)
+		] ;
+		fixDef.shape = Box2D.Collision.Shapes.b2PolygonShape.AsArray(vertices, vertices.length) ;
+		bodyDef.position.x = 9 ;
+		bodyDef.position.y = 11 ;
+		this.world.CreateBody(bodyDef).CreateFixture(fixDef);
+		
+		var vertices = [
+			new Box2D.Common.Math.b2Vec2(0, 1),
+			new Box2D.Common.Math.b2Vec2(1, 0),
+			new Box2D.Common.Math.b2Vec2(1, 2),
+			new Box2D.Common.Math.b2Vec2(0, 2)
+		] ;
+		fixDef.shape = Box2D.Collision.Shapes.b2PolygonShape.AsArray(vertices, vertices.length) ;
+		bodyDef.position.x = 11 ;
+		bodyDef.position.y = 10 ;
+		this.world.CreateBody(bodyDef).CreateFixture(fixDef);
+		
+		var vertices = [
+			new Box2D.Common.Math.b2Vec2(0, 4),
+			new Box2D.Common.Math.b2Vec2(2, 0),
+			new Box2D.Common.Math.b2Vec2(2, 6),
+			new Box2D.Common.Math.b2Vec2(0, 6)
+		] ;
+		fixDef.shape = Box2D.Collision.Shapes.b2PolygonShape.AsArray(vertices, vertices.length) ;
+		bodyDef.position.x = 12;
+		bodyDef.position.y = 6 ;
+		this.world.CreateBody(bodyDef).CreateFixture(fixDef);
+		
+		var bodyDef = new Box2D.Dynamics.b2BodyDef();
+		bodyDef.type = Box2D.Dynamics.b2Body.b2_staticBody;
+		
+		var vertices = [
+			new Box2D.Common.Math.b2Vec2(0, 3),
+			new Box2D.Common.Math.b2Vec2(1, 1),
+			new Box2D.Common.Math.b2Vec2(2, 0),
+			new Box2D.Common.Math.b2Vec2(2, 12),
+			new Box2D.Common.Math.b2Vec2(0, 12)
+		] ;
+		fixDef.shape = Box2D.Collision.Shapes.b2PolygonShape.AsArray(vertices, vertices.length) ;
+		bodyDef.position.x = 14 ;
+		bodyDef.position.y = 1 ;
+		this.world.CreateBody(bodyDef).CreateFixture(fixDef);
+	},
+	initBodies : function() {
+		var bodyDef = new Box2D.Dynamics.b2BodyDef();
+		var fixDef = new Box2D.Dynamics.b2FixtureDef();
+		Actors.clear() ;
+		
+		fixDef.density = 1.0;
+		fixDef.friction = 0.9;
+		fixDef.restitution = 0.1;
+		fixDef.shape = new Box2D.Collision.Shapes.b2CircleShape(wheelRadius);
+
+		// Back wheel
+		bodyDef.type = Box2D.Dynamics.b2Body.b2_dynamicBody;
+		bodyDef.position.x = wheelRadius;
+		bodyDef.position.y = wheelRadius;
+		var backwheel = this.world.CreateBody(bodyDef);
+		backwheel.CreateFixture(fixDef);
+		this.bodies.push(backwheel) ;
+		Actors.addActor(new Wheel(this.ctx, backwheel, wheelRadius)) ;
+
+		// Front wheel
+		bodyDef.type = Box2D.Dynamics.b2Body.b2_dynamicBody;
+		bodyDef.position.x = boardLength-wheelRadius;
+		bodyDef.position.y = wheelRadius;
+		var frontwheel = this.world.CreateBody(bodyDef);
+		frontwheel.CreateFixture(fixDef);
+		this.bodies.push(frontwheel) ;
+		Actors.addActor(new Wheel(this.ctx, frontwheel, wheelRadius)) ;
+		
+		// Deck
+		fixDef.shape = new Box2D.Collision.Shapes.b2PolygonShape();
+		fixDef.shape.SetAsBox(boardLength / 2, boardThickness / 2);
+		bodyDef.type = Box2D.Dynamics.b2Body.b2_dynamicBody;
+		bodyDef.position.x = boardLength / 2;
+		bodyDef.position.y = boardThickness / 2;
+		var board = this.world.CreateBody(bodyDef);
+		board.CreateFixture(fixDef);
+		this.bodies.push(this.board) ;
+		var boardRenderer = new Board(this.ctx, board, boardLength, boardThickness) ;
+		Actors.addActor(new Rotated(this.ctx, board, boardRenderer)) ;
+
+		// Trucks		
+		var b2JointDef = new Box2D.Dynamics.Joints.b2RevoluteJointDef();
+		b2JointDef.bodyA = backwheel;
+		b2JointDef.bodyB = board;
+		b2JointDef.localAnchorA = new Box2D.Common.Math.b2Vec2(0, 0);
+		b2JointDef.localAnchorB = new Box2D.Common.Math.b2Vec2(-boardLength/2 + truckOffset, wheelRadius);
+		var joint = this.world.CreateJoint(b2JointDef);
+		this.joints.push(joint);
+		
+		b2JointDef.bodyA = frontwheel;
+		b2JointDef.bodyB = board;
+		b2JointDef.localAnchorA = new Box2D.Common.Math.b2Vec2(0, 0);
+		b2JointDef.localAnchorB = new Box2D.Common.Math.b2Vec2(boardLength/2 - truckOffset, wheelRadius);
+		joint = this.world.CreateJoint(b2JointDef);
+		this.joints.push(joint);
+		
+		board.SetPosition(new Box2D.Common.Math.b2Vec2(0.8, 1.5));
+	},
+	update : function() {
+		this.world.Step(1 / 30 // frame-rate
+		, 10 // velocity iterations
+		, 10 // position iterations
+		);
+		//this.world.DrawDebugData();
+		this.ctx.clearRect(0, 0, 640, 480);
+		Actors.step(0) ;
+		this.world.ClearForces();
+	}
+};
---
+++ experiments/util/imageloader/js/imageloader.js
@@ -0,0 +1,165 @@
+/*
+Copyright 2011 Johan Maasing
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+ 
+ /*
+ Package: Loader
+ 
+ Utility classes to load resources 
+ */
+
+/*
+Class: ImageLoader
+
+A class that creates image objects and populate them with remote data. Use this
+class by instantiating it. Then create all the image objects that are to be 
+loaded. Register callback listeners and finally start the loader. 
+After the loader has started it is not possible to add images or listeners.
+
+Constructor: 
+  progressMeter - An optional progress meter object that is to be updated with 
+                loading progress.
+*/
+function ImageLoader(progressMeter) {
+	this.loading = false ;
+	this.images = new Array() ;
+	this.listeners = new Array() ;
+	this.progressMeter = progressMeter ;
+	this.imagesProcessed = 0 ;
+	this.numberOfImages = 0  ;
+}
+
+ImageLoader.prototype = {
+	/*
+	Function: createImage
+	
+	Create an image object that is to be loaded by this loader.
+	
+	Parameters: 
+		src - The image src URL to be used to load the image.
+	*/
+	createImage : function(src) {
+		if (this.loading) {
+			throw "Loading started, can not add more images" ;
+		}
+		if (!src) {
+			throw "Must give the image src URL" ;
+		}
+		this.numberOfImages++;
+		var image = new Image() ;
+		image.onload = bind(this, this.onLoad) ;
+		image.onabort = bind(this, this.onAbort) ;
+		image.onerror = bind(this, this.onError) ;
+		this.images.push({image: image, src: src}) ;
+		return image ;
+	},
+	/*
+	Function: start
+	
+	Start loading the images. No more images can be registered through <createImage>
+	*/
+	start : function() {
+		if (!this.loading) {
+			this.loading = true ;
+			for (var n=0; n<this.images.length; n++) {
+				this.images[n].image.src=this.images[n].src ;
+			}
+		}
+	},
+	/*
+	Function: addListener
+	
+	Add a listener the is informed when all the images have been processed.
+	
+	Parameters:
+		listener - The function to call when all images have been processed.
+	*/
+	addListener : function(listener) {
+		if (listener) {
+			this.listeners.push(listener) ;
+		}
+	},
+	/*
+	Function: fireProgress
+	
+	Internal function, do not call. Issues callbacks to the progressMeter
+	*/
+	fireProgress : function() {
+		if (this.progressMeter) {
+			this.progressMeter.progress(this.imagesProcessed/this.numberOfImages) ;
+		}
+		if (this.imagesProcessed == this.numberOfImages) {
+			for (var n=0; n<this.listeners.length; n++) {
+				this.listeners[n]() ;
+			}
+		}
+	},
+	/*
+	Callback: onLoad
+	
+	Internal function, do not call. 
+	*/
+	onLoad : function(event) {
+		this.imagesProcessed++ ;
+		this.fireProgress() ;
+		if (console) {
+			console.log("Loaded : " + this.getSrc(event)) ;
+		}
+	},
+	/*
+	Callback: onAbort
+	
+	Internal function, do not call. 
+	*/
+	onAbort : function(event) {
+		this.imagesProcessed++ ;
+		this.fireProgress() ;
+		if (console) {
+			console.log("Aborted :" + this.getSrc(event)) ;
+		}
+	},
+	/*
+	Callback: onError
+	
+	Internal function, do not call. 
+	*/
+	onError : function(event) {
+		this.imagesProcessed++ ;
+		this.fireProgress() ;
+		if (console) {
+			console.log("Error :" + this.getSrc(event)) ;
+		}
+	},
+	/*
+	Function: getSrc
+	
+	Internal function, do not call
+	*/
+	getSrc : function(event) {
+		if (event) {
+			if (event.target) {
+				if (event.target.src) {
+					return event.target.src ;
+				} else {
+					return event.target ;
+				}
+			} else {
+				return event ;
+			}
+		} else {
+			return "?" ;
+		}
+	}
+};
